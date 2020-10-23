009efc850a725c3df5a0277122ec9a9f7faa9749
Kacper Krzywiec <krzywiec@gmail.com>
2020-10-23 12:37:20 +0200

    Return table dummy data

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+import json
+
 app = Flask(__name__)
+
+with open('mock_data.json') as json_file:
+    data = json.load(json_file)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return json.dumps(data)
