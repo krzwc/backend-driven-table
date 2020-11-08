490cbf745bc6896b09822e1ac698bc314edaca2e
Kacper Krzywiec <krzywiec@gmail.com>
2020-11-08 09:26:52 +0100

    Add prostgres

--- backend/README.md
+++ backend/README.md
@@ -1,7 +1,25 @@
+# Running app
+
 python3 --version
 python3 -m venv env
 source env/bin/activate
 python -m pip install -r requirements.txt
-export FLASK_ENV=development; python3 -m flask run --host=0.0.0.0
+export FLASK_ENV=development; export JWT_SECRET_KEY=hhgaghhgsdhdhdd; export FLASK_APP=run; python3 -m flask run --host=0.0.0.0
+
+albo
+
+export FLASK_ENV=development; export JWT_SECRET_KEY=hhgaghhgsdhdhdd; python3 -m run
 
 vscode: cmd + shift + p -> python: select interpreter -> select env/bin/python
+
+## pgadmin credentials:
+
+http://localhost:8080/
+user: admin@ro.ot
+password: password
+
+## Adding postgres in pgadmin
+
+![alt text](../assets/postgres-connection-details.png?raw=true)
+user: admin
+password: secret
---
+++ backend/src/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+import json
+from .config import app_config
+
+db = SQLAlchemy()
+
+
+def create_app(env_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[env_name])
+    CORS(app)
+
+    db.init_app(app)
+
+    with open('src/mock_data.json') as json_file:
+        data = json.load(json_file)
+
+    @app.route('/table-data')
+    def get_data():
+        return json.dumps({'data': data})
+
+    @app.route('/table-data/<id>', methods=['DELETE'])
+    def delete_data(id):
+        return json.dumps({'data': {'id': id}})
+
+    @app.route('/table-config', methods=['POST', 'PUT'])
+    def get_table_config():
+        if request.method == 'PUT':
+            print(request.data)
+            return request.data
+        else:
+            return json.dumps({'data': ["first_name", "last_name", "email", "gender", "ip_address"]})
+
+    return app
