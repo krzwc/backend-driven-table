a2124c7227f67128061df52a014b2a5f60df9c7c
Kacper Krzywiec <krzywiec@gmail.com>
2020-11-09 16:53:48 +0100

    Add config model

---
+++ backend/src/models/config_model.py
@@ -0,0 +1,44 @@
+from . import db
+import datetime
+
+
+class ConfigModel(db.Model):
+    """
+    Config Model
+    """
+
+    __tablename__ = 'config'
+
+    id = db.Column(db.Integer, primary_key=True)
+    table = db.Column(db.String(128), nullable=False)
+    fields = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime)
+    modified_at = db.Column(db.DateTime)
+
+    def __init__(self, data):
+        self.table = data.get('table')
+        self.fields = data.get('fields')
+        self.created_at = datetime.datetime.utcnow()
+        self.modified_at = datetime.datetime.utcnow()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, data):
+        for key, item in data.items():
+            setattr(self, key, item)
+        self.modified_at = datetime.datetime.utcnow()
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        de.session.commit()
+
+    @staticmethod
+    def get_all_config():
+        return ConfigModel.query.all()
+
+    @staticmethod
+    def get_one_config(id):
+        return ConfigModel.query.get(id)
